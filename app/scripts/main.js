ecbed024d10731a8a5491db4b0ad03a1532131f2
Julian Fields <oregonjuice@gmail.com>
2015-10-18 10:54:51 -0700

    added some js code, just started

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$( document ).ready(function() {
+  $('.learn-more').click(function(e) {
+    e.preventDefault();
+    $(this).parents('.card').addClass('open-image');
+  });
+  $('.close').click(function(e) {
+    e.preventDefault();
+    $(this).parents('.card').removeClass('open-image');
+  });
+});

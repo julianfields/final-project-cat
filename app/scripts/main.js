6d45b1c01fe394134957041db48efca47c2f5cb9
Casey Mcvay <mcfacemail@gmail.com>
2015-10-23 11:58:47 -0700

    just checking

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+$( document ).ready(function() {
+  $('.btn adopt-button').click(function(e) {
+    e.preventDefault();
+    $(this).parents('.flexboxz').addClass('');
+  });
+  $('.close').click(function(e) {
+    e.preventDefault();
+    $(this).parents('.flexboxz').removeClass('');
+  });
+});
+
+
+// $('.readMore').on('click', function(event){
+//   event.preventDefault();
+//   var box = $(this).parents('.card');
+//   box.toggleClass('open');
+// });
